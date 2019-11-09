ae4700fadac58fe244bdb4a2ad7194d664a73c4a
vytautas.arminas <vytautas.arminas@gmai.com>
2019-11-10 00:41:57 +0100

    Add firebase service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FcmService } from './shared/service/fcm.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private fcm: FcmService,
+    public toastController: ToastController
   ) {
     this.initializeApp();
   }
@@ -22,6 +25,27 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.notificationSetup();
     });
+  }
+
+  private async presentToast(message) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  private notificationSetup() {
+    this.fcm.getToken();
+    this.fcm.onNotifications().subscribe(
+      (msg) => {
+        if (this.platform.is('ios')) {
+          this.presentToast(msg.aps.alert);
+        } else {
+          this.presentToast(msg.body);
+        }
+      });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,43 @@ import { Camera } from '@ionic-native/camera/ngx';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
+// import { IonicStorageModule } from '@ionic/storage';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FcmService } from './shared/service/fcm.service';
 
-
+const config = {
+  apiKey: 'AIzaSyAGC3iiA4sbw5YKgYL9i0RMXLelBFfBFtg',
+  authDomain: 'ionic-push-notifications-81a15.firebaseapp.com',
+  databaseURL: 'https://ionic-push-notifications-81a15.firebaseio.com',
+  projectId: 'ionic-push-notifications-81a15',
+  storageBucket: 'ionic-push-notifications-81a15.appspot.com',
+  messagingSenderId: '262059065499',
+  appId: '1:262059065499:web:67b655c0ae0edabd8c863e',
+  measurementId: 'G-V86LMNCR0D'
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    // IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
     Camera,
+    FcmService,
+    Firebase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
